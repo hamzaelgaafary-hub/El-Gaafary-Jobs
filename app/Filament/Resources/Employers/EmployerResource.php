a85d33229e54d49198ec94866b6fc6dad76016f6
hamza <hamzakhaledelgaafary@gmail.com>
2026-02-16 21:59:17 +0200

    test first commit after pull

--- app/Filament/Resources/Employers/EmployerResource.php
+++ app/Filament/Resources/Employers/EmployerResource.php
@@ -18,6 +18,8 @@ class EmployerResource extends Resource
 {
     protected static ?string $model = Employer::class;
 
+
+    
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
     public static function form(Schema $schema): Schema
